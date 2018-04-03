c3a47183a39e095a071762079c7543e40d0cd2ed
kyraz4 <kyraz4@mail.ru>
2018-04-03 23:51:51 +0400

    финал-3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 var link = document.querySelector(".write-us-btn");
 var popup = document.querySelector(".write-us-box");
+var close = document.querySelector(".button-close");
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("write-us-box-show");
 });
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("write-us-box-show");
+});
+ 
